17dfb17ae55f3d7d89571ddaea4d150884d5535e
Aaron Kyle Cuizon <aaron.kyle.cuizon@gmail.com>
2024-07-18 00:19:18 +0800

    define mutex and pass

--- testapp.rb
+++ testapp.rb
@@ -5,20 +5,22 @@ require 'logger'
 Dir[File.join(File.dirname(__FILE__), 'app', 'models', '*.rb')].each { |file| require file }
 
 set :logger, Logger.new("log/puller.log")
+set :mutex, Mutex.new
 
 get '/' do
-  logger.info "Testing"
   'Hello World'
 end
 
 post '/crawlbase-webhook' do
   if request.user_agent == "Crawlbase Monitoring Bot 1.0"
-    logger.info "Crawlbase Monitoring: #{request.user_agent}"
+    settings.mutex.synchronize do
+      logger.info "Crawlbase Monitoring: #{request.user_agent}"
+    end
 
     status 200
   else
     # process here
-    cp = CrawlerPuller.new(request.body)
+    cp = CrawlerPuller.new(request.body, settings.mutex)
     cp.process
 
     status 200
